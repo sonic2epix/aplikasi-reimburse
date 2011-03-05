f5a3558597cd47f472ba075ce500aecf55aea3b5
Fanani M. Ihsan <contact@fanani.net>
2011-03-05 18:11:03 +0700

    add domain project

---
+++ trunk/com.artivisi.reimburse.domain/src/com/artivisi/reimburse/domain/MasterPegawai.java
@@ -0,0 +1,69 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package com.artivisi.reimburse.domain;
+
+import java.io.Serializable;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+/**
+ *
+ * @author fanani
+ */
+
+@Entity
+@Table(name="reimburse_pegawai")
+public class MasterPegawai implements Serializable {
+    @Id
+    @GeneratedValue
+    private Long id ;
+
+    @Column(name="kode_pegawai")
+    private String kode ;
+
+    @Column(name="nama_pegawai")
+    private String nama ;
+
+    @Column(name="jabatan")
+    private String jabatan ;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getKode() {
+        return kode;
+    }
+
+    public void setKode(String Kode) {
+        this.kode = Kode;
+    }
+
+    public String getJabatan() {
+        return jabatan;
+    }
+
+    public void setJabatan(String jabatan) {
+        this.jabatan = jabatan;
+    }
+
+    public String getNama() {
+        return nama;
+    }
+
+    public void setNama(String nama) {
+        this.nama = nama;
+    }
+
+
+}
---
+++ trunk/com.artivisi.reimburse.domain/src/com/artivisi/reimburse/domain/MasterPlafon.java
@@ -0,0 +1,85 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package com.artivisi.reimburse.domain;
+
+import java.util.Date;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+
+/**
+ *
+ * @author fanani
+ */
+
+@Entity
+@Table(name="reimburse_plafon")
+public class MasterPlafon {
+    @Id
+    @GeneratedValue
+    private Long id ;
+    
+    @Column(name="kode")
+    private String Kode ;
+
+    @Column(name="plafong")
+    private Integer plafon ;
+
+    @Column(name="tahun")
+    @Temporal(javax.persistence.TemporalType.DATE)
+    private Date tahun ;
+
+    @ManyToOne
+    @JoinColumn(name="pegawai" ,referencedColumnName="id", nullable=false)
+    private MasterPegawai masterPegawai ;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getKode() {
+        return Kode;
+    }
+
+    public void setKode(String Kode) {
+        this.Kode = Kode;
+    }
+
+    public MasterPegawai getMasterPegawai() {
+        return masterPegawai;
+    }
+
+    public void setMasterPegawai(MasterPegawai masterPegawai) {
+        this.masterPegawai = masterPegawai;
+    }
+
+    public Integer getPlafon() {
+        return plafon;
+    }
+
+    public void setPlafon(Integer plafon) {
+        this.plafon = plafon;
+    }
+
+    public Date getTahun() {
+        return tahun;
+    }
+
+    public void setTahun(Date tahun) {
+        this.tahun = tahun;
+    }
+
+    
+}
